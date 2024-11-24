38147932f3e14ebbeb0489f8040be647025f3c32
Adi Moskovich <adimos1995@gmail.com>
2024-11-24 18:55:50 +0200

    GreedyAI,RandomAI

--- GreedyAI.java
+++ GreedyAI.java
@@ -1,11 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class GreedyAI extends AIPlayer{
     public GreedyAI(boolean isPlayerOne) {
         super(isPlayerOne);
     }
 
     @Override
-    public Move makeMove(PlayableLogic gameStatus) {
-        //TODO
+    public Move makeMove(PlayableLogic gameStatus) { //TODO ask what does normal distribution means
+        List<Position> moves = ValidMoves();
+        List<Position> greedyMoves=new ArrayList<Position>(); //a list of all the greedy moves
+        int mostFlips = 0;
+        for (int i=0; i=ValidMoves().size();i++) {
+            Position currentPos = ValidMoves().get(i);
+            int numOfMoves = greedyMoves.get(i).countFlips(currentPos); //GameLogic methode
+            if(numOfMoves>mostFlips) {
+                List<Position> greedyMoves=new ArrayList<Position>(); //reset the greedy moves
+                greedyMoves.add(currentPos);
+                mostFlips = numOfMoves;
+            }
+            if(numOfMoves=mostFlips) {
+                greedyMoves.add(currentPos);
+            }
+
+
+
+        }
+        }
         return null;
     }
 }
--- RandomAI.java
+++ RandomAI.java
@@ -1,14 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class RandomAI extends AIPlayer{
-
-
     public RandomAI(boolean isPlayerOne) {
         super(isPlayerOne);
     }
 
+    //Getters
+    public int getNumOfBombs(){
+        return this.number_of_bombs;
+    }
+    public int getNumOfUnflippedable(){
+        return this.number_of_unflippedable;
+    }
+
     @Override
     public Move makeMove(PlayableLogic gameStatus) {
+        List<Position> moves = ValidMoves();
+        if (moves.size()>0) {
+            int numOfMoves = moves.size();  //range to pick a random move from
+            Random rand = new Random();     //creating an instance of Random
+            int randomMoveIndex = rand.nextInt(numOfMoves);    //picking the random move index
+            Position randomPosition = moves.get(randomMoveIndex); //picks the random move
 
-        //TODO
+            List<String> availableDiscs = new ArrayList<>(); //creating a list of available bombs
+            availableDiscs.add("simple"); //adds a simple disc to available discs
+            if (getNumOfBombs()>0){ availableDiscs.add("bomb");} //adds a bomb to available discs
+            if (getNumOfUnflippedable()>0){availableDiscs.add("unflippedable");} //adds an unflippedable disc to the list
+            int randomDiscIndex = rand.nextInt(availableDiscs.size()); //picking a random index from the list
+            switch (availableDiscs.get(randomDiscIndex)) {
+                case "simple" -> {
+                    SimpleDisc disc = new SimpleDisc(this);
+                    Move move = new Move(this,disc,randomPosition);
+                    return move;
+                    break;
+                }
+                case "bomb" -> {
+                    BombDisc disc = new BombDisc(this);
+                    Move move = new Move(this,disc,randomPosition);
+                    return move;
+                    break;
+                }
+                case "unflippedable" -> {
+                    UnflippableDisc disc = new UnflippableDisc(this);
+                    Move move = new Move(this,disc,randomPosition);
+                    return move;
+                    break;
+                }
+            }
+
+
+
+        }
         return null;
     }
 }
