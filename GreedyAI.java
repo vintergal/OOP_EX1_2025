5a50db1fbf2fc1fcc59abfd707a55712ca598266
gal vinter <vintergal@gmail.com>
2024-11-28 14:13:45 +0200

    removed the commented previous code in makemove in greedyAi

--- GreedyAI.java
+++ GreedyAI.java
@@ -10,30 +10,6 @@ public class GreedyAI extends AIPlayer{
         List<Position> greedyMoves=new ArrayList<Position>(); //a list of all the greedy moves
         int mostFlips = 0;
 
-        /* for (int i=0; i<moves.size();i++) {
-            Position currentPos = new Position(moves.get(i).row(),moves.get(i).col());
-            int numOfFlips = gameStatus.countFlips(currentPos);
-            if(numOfFlips>mostFlips) {
-                greedyMoves.clear(); //reset the greedy moves
-                greedyMoves.add(currentPos);
-                mostFlips = numOfFlips;
-            }
-            if(numOfFlips==mostFlips) {
-                greedyMoves.add(currentPos);
-            }
-        }
-        Position greedyPos = new Position (greedyMoves.get(0).row(),greedyMoves.get(0).col());
-        for(int i=1;i<greedyMoves.size();i++) {
-            if(greedyMoves.get(i).isRightTo(greedyPos)) {
-                greedyPos = greedyMoves.get(i);
-            }
-            if(greedyMoves.get(i).col()==greedyPos.col() && greedyMoves.get(i).isUnder(greedyPos)){
-                greedyPos = greedyMoves.get(i);
-            }
-        }
-        SimpleDisc disc = new SimpleDisc(this);
-        return new Move((GameLogic) gameStatus,disc,greedyPos); */
-
         Comparator<Position> comparatorByPosition= new Comparator<Position>() {
             @Override
             public int compare(Position o1, Position o2) {
